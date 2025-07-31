898e980620018727e9cf8825244f7a7100c0322c
DivyamVanjara <140237110+DivyamVanjara@users.noreply.github.com>
2025-07-31 22:54:54 +0530

    app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.markdown("""
     }
     
     .cell-result {
-        background: white;
+        background: grey;
         padding: 1.5rem;
         border-radius: 10px;
         box-shadow: 0 2px 4px rgba(0,0,0,0.1);
